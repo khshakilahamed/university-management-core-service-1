676e65f407e0e4cfe532142b97c95e3891a8c80e
khshakilahamed <khshakil.ahamed18@gmail.com>
2023-08-20 18:12:13 +0600

    academic semester

---
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -0,0 +1,54 @@
+import { AcademicSemester } from '@prisma/client';
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
+import sendResponse from '../../../shared/sendResponse';
+import { AcademicSemesterFilterableFields } from '../academicSemester.constants';
+import { AcademicSemesterService } from './academicSemester.service';
+
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const result = await AcademicSemesterService.insertIntoDB(req.body);
+
+  sendResponse<AcademicSemester>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester Created!',
+    data: result,
+  });
+});
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, AcademicSemesterFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+
+  console.log('filters:', filters);
+  console.log('options:', options);
+
+  const result = await AcademicSemesterService.getAllFromDB(filters, options);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester data fetched!',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const getDataById = catchAsync(async (req: Request, res: Response) => {
+  const result = await AcademicSemesterService.getDataById(req.params.id);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester data fetched!',
+    data: result,
+  });
+});
+
+export const AcademicSemesterController = {
+  insertIntoDB,
+  getAllFromDB,
+  getDataById,
+};
