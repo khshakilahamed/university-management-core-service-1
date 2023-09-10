3d2e85fa868659556fac6731fdec84034e971900
khshakilahamed <khshakil.ahamed18@gmail.com>
2023-09-11 02:27:00 +0600

    studentEnrolledCourse

--- src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.controller.ts
+++ src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.controller.ts
@@ -1,8 +1,28 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
 import sendResponse from '../../../shared/sendResponse';
+import { studentEnrolledCourseMarkFilterableFields } from './studentEnrolledCourseMark.constants';
 import { StudentEnrolledCourseMarkService } from './studentEnrolledCourseMark.service';
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, studentEnrolledCourseMarkFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+
+  const result = await StudentEnrolledCourseMarkService.getAllFromDB(
+    filters,
+    options
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student course marks fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
 
 const updateStudentMarks = catchAsync(async (req: Request, res: Response) => {
   const result = await StudentEnrolledCourseMarkService.updateStudentMarks(
@@ -17,4 +37,21 @@ const updateStudentMarks = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
-export const StudentEnrolledCourseMarksController = { updateStudentMarks };
+const updateFinalMarks = catchAsync(async (req: Request, res: Response) => {
+  const result = await StudentEnrolledCourseMarkService.updateFinalMarks(
+    req.body
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Final Marks updated',
+    data: result,
+  });
+});
+
+export const StudentEnrolledCourseMarksController = {
+  getAllFromDB,
+  updateStudentMarks,
+  updateFinalMarks,
+};
--- src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.route.ts
+++ src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.route.ts
@@ -1,11 +1,24 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import { StudentEnrolledCourseMarksController } from './studentEnrolledCourseMark.controller';
 
 const router = express.Router();
 
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.FACULTY),
+  StudentEnrolledCourseMarksController.getAllFromDB
+);
+
 router.patch(
   '/update-marks',
   StudentEnrolledCourseMarksController.updateStudentMarks
+);
+
+router.patch(
+  '/update-final-marks',
+  StudentEnrolledCourseMarksController.updateFinalMarks
 );
 
 export const studentEnrolledCourseMarkRoutes = router;
